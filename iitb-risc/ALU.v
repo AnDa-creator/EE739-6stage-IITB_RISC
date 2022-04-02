cd5db9a005f01785473e7471eaa513b87869bcd0
AnDa-creator <anuranan.fifa14@gmail.com>
2022-04-02 21:46:24 +0530

    Added ALU and ALU Control

---
+++ iitb-risc/ALU.v
@@ -0,0 +1,33 @@
+module alu(       
+      input          [15:0]     a,              //src1  
+      input          [15:0]     b,              //src2  
+      input          [1:0]     alu_control,     //Control signal for ALU  
+      output     reg     [15:0]     result,     //result       
+      output reg zero,                          //zero
+      output reg carry                          //carry
+   );
+
+ reg[16:0] total_sum;
+
+ initial begin
+    zero = 1'b0;
+    carry = 1'b0;
+    result = 0;
+ end
+ always @(*)  
+ begin   
+      case(alu_control)  
+      2'b00: total_sum = {1'b0,a} + {1'b0,b};   // add  
+      2'b01: result = ~(a & b);                 // Nand  
+      default:result = 0;                       // NOP
+      endcase  
+ end
+ always @(*) begin
+    result = (alu_control == 2'b00) ? total_sum[15:0]:result ;                       //result
+    zero = (result==16'd0) ? 1'b1: 1'b0;                                             //zero
+    carry = (total_sum[16]==1'b1) ? 1'b1: 1'b0;                                      //carry
+ end  
+ 
+ 
+
+ endmodule  
---
+++ iitb-risc/aluCtrl.v
@@ -0,0 +1,36 @@
+module ALUControl( ALU_Control, ALUOp, Function, carry_in, zero_in);
+output reg[1:0] ALU_Control;  
+input [3:0] ALUOp;  
+input [1:0] Function;
+input carry_in, zero_in;
+
+wire [5:0] ALUControlIn; 
+assign ALUControlIn = {ALUOp,Function}; 
+
+parameter ADD = 6'b000100;
+parameter ADC = 6'b000110;
+parameter ADZ = 6'b000101;
+parameter ADL = 6'b000111;
+parameter ADI = 4'b0000;
+parameter NDU = 6'b001000;
+parameter NDC = 6'b001010;
+parameter NDZ = 6'b001001; // ADD anything we want later
+
+
+ always @(ALUControlIn)  
+ case (ALUControlIn)  
+  ADD: ALU_Control = 2'b00;                           // ADD
+  ADC: ALU_Control = (carry_in == 1) ? 2'b00: 2'b10;  // if carry_in is 1, then ALU_Control = 2'b00, otherwise 2'b10
+  ADZ: ALU_Control = (zero_in == 1) ? 2'b00: 2'b10;   // if zero_in is 1, then ALU_Control = 2'b00, otherwise 2'b10
+  ADL: ALU_Control =  2'b00;                          // ADD
+  {ADI,2'b00}: ALU_Control = 2'b00;                   // ADD
+  NDU: ALU_Control= 2'b01;                            // NAND
+  NDC: ALU_Control=  (carry_in == 1) ? 2'b01: 2'b10;  // if carry_in is 1, then ALU_Control = 2'b01, otherwise 2'b10
+  NDZ: ALU_Control=  (zero_in == 1) ? 2'b01: 2'b10;   // if zero_in is 1, then ALU_Control = 2'b01, otherwise 2'b10
+  default: ALU_Control=3'b010;                        // NOP
+  endcase  
+
+
+endmodule  
+
+
