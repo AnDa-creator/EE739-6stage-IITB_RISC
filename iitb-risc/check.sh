41cbd2c18055cf6d6a373c33b38c192375f5b6c3
Aditya Rajeev Harakare <46088878+adityaharakare1@users.noreply.github.com>
2022-04-07 16:20:17 +0530

    updates check.sh

--- iitb-risc/check.sh
+++ iitb-risc/check.sh
@@ -15,3 +15,18 @@ iverilog -o iitb_risc mux.v
 
 echo "Checking registerBank.v"
 iverilog -o iitb_risc registerBank.v
+
+echo "Checking IF2ID_Stage.v"
+iverilog -o iitb_risc IF2ID_Stage.v
+
+echo "Checking ID2RR_Stage.v"
+iverilog -o iitb_risc ID2RR_Stage.v
+
+echo "Checking RR2EX_Stage.v"
+iverilog -o iitb_risc RR2EX_Stage.v
+
+echo "Checking EX2MM_Stage.v"
+iverilog -o iitb_risc RR2MM_Stage.v
+
+echo "Checking MM2WB_Stage.v"
+iverilog -o iitb_risc MM2WB_Stage.v
