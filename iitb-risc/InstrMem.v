5ea94764a1825cdb051091c51b5292a9c6734460
Ruchirchheda <44035016+Ruchirchheda@users.noreply.github.com>
2022-04-07 12:13:19 +0530

    indenting the code

--- iitb-risc/InstrMem.v
+++ iitb-risc/InstrMem.v
@@ -1,33 +1,35 @@
 // Verilog code for instruction memory
- module instr_mem          // a synthesisable rom implementation  
- (  
-      input     [15:0]     pc,  
-      output wire     [15:0]          instruction  
- );  
-      wire [3 : 0] rom_addr = pc[4 : 1];  
-      /*   
-      NOP
-      addi $r1, $r1, 7;
-      addi $r2, $r2, 5;
-      add  $r3, $r1, $r2;
-      mul  $r4, $r1, $r2;
-      addi $r5, $r5, 1;
-      sll $r6, $r1, $r5; 
-      sll_test $r7, $r1, $r5;
- */  
-      reg [15:0] rom[16:0];  
-      initial  
-      begin
-          
-                rom[0] = 16'b0000000000000000; // NOP
-                rom[1] = 16'b1110010010000111; // addi $r1, $r1, 7; 
-                rom[2] = 16'b1110100100000101; // addi $r2, $r2, 5;
-                rom[3] = 16'b0000100010110000; // add  $r3, $r1, $r2;
-                rom[4] = 16'b0000100011000101; // mul  $r4, $r1, $r2;
-                rom[5] = 16'b1111011010000001; // addi $r5, $r5, 1;
-                rom[6] = 16'b0000011011100110; // sll $r6, $r1, $r5;
-                rom[7] = 16'b0000011011110111; // srl $r7, $r1, $r5;
-                 
-      end  
-      assign instruction = (pc[15:0] < 48 )? rom[rom_addr[3:0]]: 16'd0;  
- endmodule   
+ module instr_mem          // a synthesisable rom implementation
+ (
+    input   [15:0]  pc,
+    output wire [15:0]  instruction
+ );
+    wire [3:0] rom_addr = pc[4:1];
+
+/*
+    NOP
+    addi $r1, $r1, 7;
+    addi $r2, $r2, 5;
+    add  $r3, $r1, $r2;
+    mul  $r4, $r1, $r2;
+    addi $r5, $r5, 1;
+    sll $r6, $r1, $r5;
+    sll_test $r7, $r1, $r5;
+ */
+    reg [15:0] rom [16:0];
+
+    initial
+    begin
+        rom[0] = 16'b0000000000000000; // NOP
+        rom[1] = 16'b1110010010000111; // addi $r1, $r1, 7;
+        rom[2] = 16'b1110100100000101; // addi $r2, $r2, 5;
+        rom[3] = 16'b0000100010110000; // add  $r3, $r1, $r2;
+        rom[4] = 16'b0000100011000101; // mul  $r4, $r1, $r2;
+        rom[5] = 16'b1111011010000001; // addi $r5, $r5, 1;
+        rom[6] = 16'b0000011011100110; // sll $r6, $r1, $r5;
+        rom[7] = 16'b0000011011110111; // srl $r7, $r1, $r5;
+    end
+
+    assign instruction = (pc[15:0] < 48 ) ? rom[rom_addr[3:0]]: 16'd0;
+
+endmodule
--- iitb-risc/dataMem.v
+++ iitb-risc/dataMem.v
@@ -1,32 +1,37 @@
  // SubModule :: Data Memory
- module data_memory  
- (  
-      input                         clk,              // clock
-      input                         rst,              // reset  signal
-      input     [15:0]              mem_access_addr,  // address input, shared by read and write port
-      input     [15:0]              mem_write_data,   // data to be written
-      input                         mem_write_en,     // enable write port 
-      input                         mem_read,         // enable read port
-      output     [15:0]             mem_read_data  
- );  
-      integer i;  
-      reg [15:0] ram [4096:0];  
-      wire [11 : 0] ram_addr = mem_access_addr[11 : 0]; 
+ module data_memory
+ (
+    input           clk,              // clock
+    input           rst,              // reset  signal
+    input   [15:0]  mem_access_addr,  // address input, shared by read and write port
+    input   [15:0]  mem_write_data,   // data to be written
+    input           mem_write_en,     // enable write port
+    input           mem_read,         // enable read port
+    output  [15:0]  mem_read_data
+ );
 
-      initial begin  
-           for(i=0;i<4096;i=i+1)  
-                ram[i] <= 16'd0;  
-      end  
-      always @(posedge clk) begin  
-           if (mem_write_en && ~rst) begin 
-                ram[ram_addr] <= mem_write_data;  
-      end
+    integer i;
+    reg     [15:0] ram [4096:0];
+    wire    [11:0] ram_addr = mem_access_addr [11:0];
 
-      always @(posedge clk) begin
-          if (rst) begin
-              for(i=0;i<4096;i=i+1)  
-                ram[i] <= 16'd0; 
-      end  
-      
-      assign mem_read_data = (mem_read==1'b1) ? ram[ram_addr]: 16'd0;   
- endmodule  
+    initial
+    begin
+        for(i=0; i<4096; i=i+1)
+            ram[i] <= 16'd0;
+    end
+
+    always @(posedge clk)
+    begin
+        if (mem_write_en && ~rst) 
+            ram[ram_addr] <= mem_write_data;
+
+        always @(posedge clk) begin
+            if (rst) 
+            begin
+                for(i=0; i<4096; i=i+1)
+                    ram[i] <= 16'd0;
+            end
+    end
+      assign mem_read_data = (mem_read == 1'b1) ? ram[ram_addr]: 16'd0;
+
+endmodule
